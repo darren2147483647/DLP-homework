4fe880f4ca74dbc13f78753e8af842c3b03acdc6
darren2147483647 <bestgreenie@gmail.com>
2024-07-31 15:49:21 +0800

    Add files via upload
    2024/7/31
    dataset is too big to upload
    easy to overfitting

---
+++ lab2/model/SCCNet.py
@@ -0,0 +1,54 @@
+# implement SCCNet model
+
+import torch
+import torch.nn as nn
+
+# reference paper: https://ieeexplore.ieee.org/document/8716937
+class SquareLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, x):
+        pass
+
+class SCCNet(nn.Module):
+    def __init__(self, numClasses=0, timeSample=0, Nu=0, C=0, Nc=0, Nt=0, dropoutRate=0):
+        super(SCCNet, self).__init__()
+        self.N=numClasses
+        self.T=timeSample
+        self.C=C
+        self.dropoutRate=dropoutRate
+        self.Nu=Nu
+        self.Nc=Nc
+        self.Nt=Nt
+        self.conv1=nn.Conv2d(1,Nu,(C,Nt))
+        self.norm1=nn.BatchNorm2d(Nu)
+        self.conv2=nn.Conv2d(Nu,Nc,(1,12))#lazy conv permutation
+        self.norm2=nn.BatchNorm2d(Nc)
+        self.drop=nn.Dropout(p=dropoutRate)
+        self.pool=nn.AvgPool2d((1,62),ceil_mode=True)
+        self.flat=nn.Flatten()
+        self.linear=nn.Linear(Nc*((timeSample-12+1)//62+1),4)#ceil
+        return
+        pass
+
+    def forward(self, x):
+        self.x=x
+        #assert x.shape==(x.shape[0],1,self.C,self.T)
+        self.z1=self.conv1(self.x)
+        self.z2=self.norm1(self.z1)
+        self.z3=self.z2**2
+        self.z4=self.conv2(self.z3)
+        self.z5=self.norm2(self.z4)
+        self.z6=self.z5**2
+        self.z7=self.drop(self.z6)
+        self.z8=self.pool(self.z7)
+        self.z9=self.flat(self.z8)
+        self.o=self.linear(self.z9)
+        self.y=torch.argmax(self.o,1)
+        return self.o
+        pass
+
+    # if needed, implement the get_size method for the in channel of fc layer
+    def get_size(self, C, N):
+        return self.x[N,C]
+        pass
---
+++ lab2/trainer.py
@@ -0,0 +1,98 @@
+# implement your training script here
+from Dataloader import *
+from model.SCCNet import *
+from utils import *
+import numpy as np
+
+modelist=["SD","LOSO","LOSO-FT"]
+mode=modelist[0]
+
+todolist=["train","test"]
+todo=todolist[0]
+
+if __name__=="__main__":
+    lr=0.01
+    epochs=250
+    batch_size=128
+    train_data=MIBCI2aDataset("train",mode,batch_size)
+    test_data=MIBCI2aDataset("test",mode)
+    ft_data=MIBCI2aDataset("finetune",mode)
+    model=SCCNet(numClasses=train_data.features[0].shape[0],C=train_data.features[0].shape[2],timeSample=train_data.features[0].shape[3],Nu=22,Nc=20,Nt=1,dropoutRate=0.5)
+    optimizer=torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0001)
+    scheduler=torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0 = 20, T_mult = 1, eta_min = 8e-6, last_epoch = -1)
+    loss_func=nn.CrossEntropyLoss()
+    if todo=="test":
+        model.load_state_dict(torch.load(f"model_weight_{mode}.pt"))
+        model.eval()
+        model.forward(test_data.features)
+        loss=loss_func(model.o,test_data.labels)
+        accuracy=torch.eq(model.y,test_data.labels).sum().item()/test_data.labels.shape[0]
+        print(f"test | loss:{loss.item()} accuracy:{accuracy}")
+    else:
+        show_span=1
+        accuracy_history=[]
+        accuracy_history2=[]
+
+        #train
+        for i in range(epochs):
+            for j in range(train_data.batch_num):
+                optimizer.zero_grad()
+                model.train()
+                model.forward(train_data.features[j])
+                loss=loss_func(model.o,train_data.labels[j])
+                accuracy=torch.eq(model.y,train_data.labels[j]).sum().item()/train_data.labels[j].shape[0]
+                loss.backward()
+                optimizer.step()
+                if i%show_span==0 and j==0:
+                    print(f"1st batch eps:{i} loss:{loss.item()} accuracy:{accuracy}")
+                    accuracy_history.append(loss.item())
+
+                    model.eval()
+                    model.forward(test_data.features)
+                    loss=loss_func(model.o,test_data.labels)
+                    accuracy=torch.eq(model.y,test_data.labels).sum().item()/test_data.labels.shape[0]
+                    accuracy_history2.append(loss.item())
+
+            scheduler.step()
+        #draw
+        draw(accuracy_history,epochs,show_span,"Loss")
+        train_test_plot(accuracy_history,accuracy_history2,epochs,show_span,"Loss")
+        #finetune
+        if mode=="LOSO-FT":
+            print("===finetune===")
+            ftep=100
+            cnt=0
+            optimizer=torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0001)
+            for param in model.parameters():
+                if cnt==6:
+                    break
+                param.requires_grad = False
+                cnt+=1
+            accuracy_history=[]
+            model.train()
+            for i in range(ftep):
+                optimizer.zero_grad()
+                model.forward(ft_data.features)
+                loss=loss_func(model.o,ft_data.labels)
+                accuracy=torch.eq(model.y,ft_data.labels).sum().item()/ft_data.labels.shape[0]
+                if i%show_span==0:
+                    print(f"eps:{i} loss:{loss.item()} accuracy:{accuracy}")
+                    accuracy_history.append(accuracy)
+                loss.backward()
+                optimizer.step()
+            draw(accuracy_history,ftep,show_span,"Loss")
+            print("===end===")
+        #test
+        model.eval()
+        model.forward(test_data.features)
+        loss=loss_func(model.o,test_data.labels)
+        accuracy=torch.eq(model.y,test_data.labels).sum().item()/test_data.labels.shape[0]
+        print(f"test | loss:{loss.item()} accuracy:{accuracy}")
+        #save modelist=["SD","LOSO","LOSO-FT"]
+        if mode=="SD" and accuracy>0.6:
+            torch.save(model.state_dict(),f"model_weight_{mode}.pt")
+        if mode=="LOSO" and accuracy>0.5:
+            torch.save(model.state_dict(),f"model_weight_{mode}.pt")
+        if mode=="LOSO-FT" and accuracy>0.7:
+            torch.save(model.state_dict(),f"model_weight_{mode}.pt")
+    ###
